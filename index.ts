1450eac8a176e581b613f18c747450ecf6b8ed1b
Henri Väisänen <henri.vaisanen@tietoevry.com>
2023-01-10 16:42:23 +0200

    Remove unnecessary stuff from index.ts

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
 import http from 'http';
 import express, { Express } from 'express';
 const app: Express = express();
-import mongoose from 'mongoose';
 import config from './utils/config';
-
-const mongoUrl = 'mongodb://localhost/bloglist';
-mongoose.connect(mongoUrl);
 
 const server = http.createServer(app);
 
