9da1f353fea939227043dd8322555affaf19ec4e
Henri Väisänen <henri.vaisanen@tietoevry.com>
2023-01-10 16:00:52 +0200

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'commonjs': false,
+    'es2021': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'overrides': [
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'plugins': [
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2
+    ],
+    'linebreak-style': [
+      'error',
+      'unix'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    'eqeqeq': 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': [
+      'error', 'always'
+    ],
+    'arrow-spacing': [
+      'error', { 'before': true, 'after': true }
+    ],
+    '@typescript-eslint/no-var-requires': 0,
+  }
+};
