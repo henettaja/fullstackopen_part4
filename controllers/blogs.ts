0000180a984451737fe57986a8898d52e5e2f163
Henri Väisänen <henri.vaisanen@tietoevry.com>
2023-01-25 14:55:17 +0200

    Implement functionality to delete a blog from api

--- controllers/blogs.ts
+++ controllers/blogs.ts
@@ -1,10 +1,11 @@
 import 'express-async-errors';
-const blogsRouter = require('express').Router();
 import logger from '../utils/logger';
 import { Blog } from '../models/blog';
 
+const blogsRouter = require('express').Router();
+
 blogsRouter.get('/', async (request, response) => {
-  logger.info('↩️ Fetching data from MongoDB');
+  logger.info('↩️ Fetching blogs from db');
   const blogs = await Blog.find({});
   response.json(blogs);
 });
@@ -18,9 +19,18 @@ blogsRouter.post('/', async (request, response) => {
     likes: request.body.likes || 0
   });
 
-  logger.info('⤴️ Posting data to MongoDB\n', request.body);
+  logger.info('⤴️ Posting a blog to db\n', request.body);
   const result = await blog.save();
   response.status(201).json(result);
+});
+
+blogsRouter.delete('/:id', async (request, response) => {
+  const idToRemove = request.params.id;
+
+  logger.info(`⌫ Deleting a blog with id ${idToRemove} from db`);
+  await Blog.deleteOne({ _id: idToRemove });
+
+  response.status(204).send();
 });
 
 export default blogsRouter;
