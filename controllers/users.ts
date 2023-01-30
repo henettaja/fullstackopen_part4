a7e60661f5408ed918cf5d2bee2469748aad9b64
Henri Väisänen <henri.vaisanen@tietoevry.com>
2023-01-30 15:08:59 +0200

    Use express-validator to catch too short passwords

--- controllers/users.ts
+++ controllers/users.ts
@@ -1,20 +1,29 @@
 import User from '../models/user';
 import logger from '../utils/logger';
 import bcrypt from 'bcrypt';
+import { body, validationResult } from 'express-validator';
 
 const usersRouter = require('express').Router();
 const saltRounds = 10;
 
-usersRouter.post('/', async (request, response) => {
-  const user = new User({
-    passwordHash: await bcrypt.hash(request.body.password, saltRounds),
-    ...request.body
-  });
+usersRouter.post('/',
+  body('password').isLength({ min:3 }),
+  async (request, response) => {
 
-  logger.info('⤴️ Creating a new user to db\n', request.body);
-  const result = await user.save();
-  response.status(201).send(result);
-});
+    const errors = validationResult(request);
+    if (!errors.isEmpty()) {
+      return response.status(400).json({ errors: errors.array() });
+    }
+
+    const user = new User({
+      passwordHash: await bcrypt.hash(request.body.password, saltRounds),
+      ...request.body
+    });
+
+    logger.info('⤴️ Creating a new user to db\n', request.body);
+    const result = await user.save();
+    response.status(201).send(result);
+  });
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({});
 
