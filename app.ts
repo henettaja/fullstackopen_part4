364819c2a96f5ebe3618f2d5c60c9ddc8c42a817
Henri Väisänen <henri.vaisanen@tietoevry.com>
2023-02-01 10:16:12 +0200

    Only use token and user extractor on blogsRouter

--- app.ts
+++ app.ts
@@ -30,9 +30,8 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.json());
 app.use(middleware.requestLogger);
-app.use(middleware.tokenExtractor);
 
-app.use('/api/blogs', middleware.userExtractor, blogsRouter);
+app.use('/api/blogs', middleware.tokenExtractor, middleware.userExtractor, blogsRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
