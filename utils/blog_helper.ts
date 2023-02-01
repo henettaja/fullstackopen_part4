77d329edf8b7cb95132823890e24bf1d841b1a14
Henri Väisänen <henri.vaisanen@tietoevry.com>
2023-02-01 10:22:51 +0200

    Fix helper constant to match type

--- utils/blog_helper.ts
+++ utils/blog_helper.ts
@@ -1,5 +1,6 @@
 import { IBlog } from '../models/blog';
 import { IAuthor } from '../models/author';
+import { Schema } from 'mongoose';
 
 export const longListOfBlogs: IBlog[] = [
   {
@@ -7,36 +8,42 @@ export const longListOfBlogs: IBlog[] = [
     author: 'Michael Chan',
     url: 'https://reactpatterns.com/',
     likes: 7,
+    user: {} as Schema.Types.ObjectId
   },
   {
     title: 'Go To Statement Considered Harmful',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
     likes: 5,
+    user: {} as Schema.Types.ObjectId
   },
   {
     title: 'Canonical string reduction',
     author: 'Edsger W. Dijkstra',
     url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
     likes: 12,
+    user: {} as Schema.Types.ObjectId
   },
   {
     title: 'First class tests',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
     likes: 10,
+    user: {} as Schema.Types.ObjectId
   },
   {
     title: 'TDD harms architecture',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
     likes: 0,
+    user: {} as Schema.Types.ObjectId
   },
   {
     title: 'Type wars',
     author: 'Robert C. Martin',
     url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
     likes: 2,
+    user: {} as Schema.Types.ObjectId
   }
 ];
 
